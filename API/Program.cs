201ee36f07788e25768c5f8c8790727cd0eda0a6
duokhongg3937 <nosurprise39@gmail.com>
2024-04-19 22:44:25 +0700

    Add CRUD api using CQRS + Mediator

--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,15 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,6 +17,7 @@ if (app.Environment.IsDevelopment())
 };
 
 app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Hanlder : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Hanlder(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
